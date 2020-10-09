1b7392db3bf85551bc3ffcb0de2f537126a96875
Jan Fredrik Øveraasen <janfredrik1@users.noreply.github.com>
2020-10-09 21:06:25 +0200

    fix

--- core/utils/ApiUrlHelper.js
+++ core/utils/ApiUrlHelper.js
@@ -3,7 +3,7 @@ import absoluteUrl from 'next-absolute-url'
 export default (path, req) => {
 
     //https://getoutputs.com
-    const {origin} = absoluteUrl(req, 'getoutputs.com');
+    const {origin} = absoluteUrl(req);
     const apiURL = `${origin}${path}`;
     return apiURL;
 };
