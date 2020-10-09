fe5de434660efdc8baab4a8c387745ae9775f65c
Jan Fredrik Øveraasen <janfredrik1@users.noreply.github.com>
2020-10-09 21:28:56 +0200

    fix

--- components/utils/seo.js
+++ components/utils/seo.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {NextSeo} from "next-seo";
 
-const defaultTitle = '';
-const defaultDescription = 'Outputs - World\'s Tiniest Strategy Tool';
+const defaultTitle = 'Outputs. ';
+const defaultDescription = '';
 const defaultOGURL = '';
 const defaultOGImage = '';
 
@@ -12,7 +12,7 @@ export default class Seo extends React.Component {
     render() {
         return (
             <NextSeo
-                title={(this.props.seo.fields.seoTitle || defaultTitle) + ' | Outputs'}
+                title={(this.props.seo.fields.seoTitle || defaultTitle) + ' | World\'s Tiniest Strategy Tool'}
                 description={this.props.seo.fields.seoDescription || defaultDescription}
             />
         )
