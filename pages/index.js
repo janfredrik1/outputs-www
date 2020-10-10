f9214ba720acf3a9563bd3b5f7279288a2327674
Jan Fredrik Øveraasen <janfredrik1@users.noreply.github.com>
2020-10-10 06:38:09 +0200

    fix

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default class PageHome extends React.Component {
 
 
                             <h2>
-                                <span class="font-weight-bold">Outputs&trade;</span> is a tiny and efficient strategy
+                                <span class="font-weight-bold">Outputs&trade;</span> is a tiny and efficient online strategy
                                 tool<br/>
                                 <span className="text-primary">that helps your organization realise its ambitions</span>.
                             </h2>
